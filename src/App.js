ee55af03711a714756999889e76396f87f892be8
Kamlesh <mekamleshk@gmail.com>
2024-04-15 23:14:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useEffect, useContext } from "react";
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+import BooksContext from "./context/books";
+
+function App() {
+  const { fetchBooks } = useContext(BooksContext);
+
+  useEffect(() => {
+    fetchBooks();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>Reading List</h1>
+      <BookList />
+      <BookCreate />
+    </div>
+  );
+}
+
+export default App;
